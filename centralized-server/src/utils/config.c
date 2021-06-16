ca9448ef353208680f19953036f7bfacd1868c85
Kevin <skryfall@gmail.com>
2021-06-16 14:50:33 -0600

    [FIX] Another malloc bugfix

--- centralized-server/src/utils/config.c
+++ centralized-server/src/utils/config.c
@@ -37,10 +37,10 @@ int readConfigFile() {
         exit(EXIT_FAILURE);
     }
 
-    node1Ip = malloc(strlen(node1IpTemp));
+    node1Ip = malloc(strlen(node1IpTemp) + 1);
     strcpy(node1Ip, node1IpTemp);
 
-    node2Ip = malloc(strlen(node2IpTemp));
+    node2Ip = malloc(strlen(node2IpTemp) + 1);
     strcpy(node2Ip, node2IpTemp);   
 
     writeToLog("Status: Successfully read config file");
